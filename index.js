a4c2037fff9e303e57ff72ebb387ab06232629b2
ljones11 <40867377+ljones11@users.noreply.github.com>
2020-05-26 08:55:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  
   console.log('Example app listening on port 3000!');
 });
